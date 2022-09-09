1ecb3a19c866b37a95675f635ae5ac1d4e4a4d38
Abdullateef Ibraheem <abdullateefoscar@gmail.com>
2022-09-08 17:12:16 -0700

    tasks345

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * This prints a set of strings using 'puts' command
+ * the main function serves as the entry point
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * This program prints the given string using the 'printf' command
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
