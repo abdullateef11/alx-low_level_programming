776c241dd8f7512a8c49ad565adb0c3ad9549bab
Abdullateef Ibraheem <abdullateefoscar@gmail.com>
2022-09-08 17:56:15 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- * This program prints the given string using the 'printf' command
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art");
+	printf("\"with proper grammar, but the outcome is a piece of art");
 	return (0);
 }
