8ba2f783955ec04a33c14f3672adbc538077abd3
Abdullateef Ibraheem <abdullateefoscar@gmail.com>
2022-09-08 17:45:04 -0700

    t4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * This prints a set of strings using 'puts' command
- * the main function serves as the entry point
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
  */
 int main(void)
 {
