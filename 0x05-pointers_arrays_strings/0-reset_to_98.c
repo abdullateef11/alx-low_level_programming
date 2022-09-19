35a588c5547da9b9375893b011b35b092c33590d
Abdullateef Ibraheem <abdullateefoscar@gmail.com>
2022-09-19 16:40:23 -0700

    mandatory

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry pointfor resetting n
+ * @n: integer pointer to a variable to be updated
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	 *n = 98;
+
+}
